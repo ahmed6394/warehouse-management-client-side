77933dca45157e831b82516f360ae2aba7f61695
mahabub ahmed <mahabub.ahmed.eee@gmail.com>
2022-05-08 01:13:55 +0200

    blog update

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -8,12 +8,13 @@ const Blog = () => {
         <Card.Body>
           <Card.Title>Difference between javascript and nodejs</Card.Title>
           <Card.Text>
-            The authorization is a process to set permission for taking any
-            action or using data. Similarly, authentication is a process to
-            check if the user is permitted or not to access the information or
-            perform any action. If an authenticated user is using your app, they
-            have identified their true identity in several ways, such as
-            providing the right credentials.
+            Javascript is a programming language that is used for writing
+            scripts on the website. Which is basically used on the client-side
+            to add HTML and play with the DOM. It is the upgraded version of
+            ECMA script that uses Chrome's V8 engine written in C++. On the
+            other hand NodeJS is a Javascript runtime environment. Which is
+            mostly used on the server-side. It has no capability to add HTML
+            tags. Nodejs is written in C, C++ and Javascript.
           </Card.Text>
         </Card.Body>
       </Card>
@@ -21,12 +22,12 @@ const Blog = () => {
         <Card.Body>
           <Card.Title>Differences between sql and nosql databases.</Card.Title>
           <Card.Text>
-            Google Firebase provides many features that pitch it as the go-to
-            backend development tool for web and mobile apps. It reduces
-            development workload and time. And it's a perfect prototyping tool.
-            Firebase is easy, lightweight, friendly, and industrially
-            recognized. There are so many authorization providers like Auth0,
-            MongoDB, Okta, Passport, keycloak etc in authorization industry.
+            RELATIONAL DATABASE MANAGEMENT SYSTEM (RDBMS). WHich is best suited
+            for complex queries not for hierarchical data storage. These
+            databases have fixed or static or predefined schema. On the
+            otherhand, Non-relational or distributed database system is best
+            suited for hierarchical data storage not so good for complex
+            queries. They have dynamic schema
           </Card.Text>
         </Card.Body>
       </Card>
@@ -36,9 +37,14 @@ const Blog = () => {
             What is the purpose of jwt and how does it work?
           </Card.Title>
           <Card.Text>
-            Along with authorization firebase also provide some features like
-            firebase database, real-time database, hosting, and machine
-            learning.
+            JSON Web Token (JWT) is an open standard (RFC 7519) that defines a
+            compact and self-contained way for securely transmitting information
+            between parties as a JSON object. This information can be verified
+            and trusted because it is digitally signed.JWTs are used as a secure
+            way to authenticate users and share information. Typically, a
+            private key, or secret, is used by the issuer to sign the JWT. The
+            receiver of the JWT will verify the signature to ensure that the
+            token hasn't been altered after it was signed by the issuer.
           </Card.Text>
         </Card.Body>
       </Card>
